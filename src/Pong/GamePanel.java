2f87735b133a857c4b06a5cb89249f5bc2c07ad0
toriningenn <annamihailova72@yandex.com>
2020-10-30 13:23:01 +0100

    2

--- src/Pong/GamePanel.java
+++ src/Pong/GamePanel.java
@@ -16,7 +16,7 @@ public class GamePanel extends JPanel implements Runnable {
     Image image;
     Graphics graphics;
     Random random;
-    Paddle padlle1;
+    Paddle paddle1;
     Paddle paddle2;
     Ball ball;
     Score score;
@@ -41,35 +41,85 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void newPaddles() {
+        paddle1 = new Paddle(0,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,1);
+        //у игрока 1 слева, поэтому x=0, у игрока 2 справа
+        paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH,(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2),PADDLE_WIDTH,PADDLE_HEIGHT,2);
+        // GAME_WIDTH-PADDLE_WIDTH чтобы сместить максимально вправо.
+        //(GAME_HEIGHT/2)-(PADDLE_HEIGHT/2) начальная y позиция
 
     }
     public void paint(Graphics g) {
         image = createImage(getWidth(),getHeight());
         graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image,0,0,this);
+                //в верхнем левом углу коррдинаты 0 0
         
 
     }
     public void draw(Graphics g) {
-
+        paddle1.draw(g);
+        paddle2.draw(g);
+        Toolkit.getDefaultToolkit().sync(); //улучшает анимацию
     }
 
     public void move() {
-
+        //будет вызывать move() после каждого цикла лупа. Это ускорит их движение
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
-    public void checkCollision() {
 
+
+    public void checkCollision() {
+//останавливает платформы по бокам экрана
+        //вверх
+        if(paddle1.y<=0) {
+            paddle1.y=0;
+        }
+        //вниз
+        if(paddle1.y >=(GAME_HEIGHT-PADDLE_HEIGHT)) {
+            paddle1.y=GAME_HEIGHT-PADDLE_HEIGHT;
+        }
+        if(paddle2.y<=0) {
+            paddle2.y=0;
+        }
+        if(paddle2.y >=(GAME_HEIGHT-PADDLE_HEIGHT)) {
+            paddle2.y=GAME_HEIGHT-PADDLE_HEIGHT;
+        }
     }
     public void run() {
+        //game loop
+        long lastTime = System.nanoTime(); //время работы JVM, этим можно измерить время выполнения кода
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while(true) {
+            long now = System.nanoTime();
+            delta += (now-lastTime)/ns;
+            lastTime = now;
+            if(delta >=1) {
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
 
     }
 
     //action listener
+
     public class AL extends KeyAdapter {
-        public void KeyPressed(KeyEvent e) {
-
+        //переназначаем методы KeyAdapter. AL потом используется в конструкторе панели this.addKeyListener(new AL());
+        public void keyPressed(KeyEvent e) {
+            paddle1.keyPressed(e);
+            paddle2.keyPressed(e);
         }
-        public void KeyReleased(KeyEvent e) {
 
+        public void keyReleased(KeyEvent e) {
+            paddle1.keyReleased(e);
+            paddle2.keyReleased(e);
         }
 
     }
--- src/Pong/Paddle.java
+++ src/Pong/Paddle.java
@@ -1,34 +1,92 @@
 package Pong;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
 
-
-public class Paddle extends  Rectangle {
+public class Paddle extends Rectangle {
 
     int id; //для игрока 1 и игрока 2
-    int yVelocity; //скорость с которой двигается платформа
+    int yVelocity;
+    int speed = 10;
+    //скорость с которой двигается платформа
 
-    public Paddle() {
+    public Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id) {
+        super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT); //super в данном случае будет конструктором из rectangle, где уже есть
+        //все эти значения кроме id, поэтому они автоматически есть и у Paddle
+        this.id = id;
     }
 
     public void keyPressed(KeyEvent e) {
+        switch (id) {
+            //только первая педаль
+            case 1:
+                //если нажимаем W - запускается этот код
+                if (e.getKeyCode() == KeyEvent.VK_W) {
+                    setYDirection(-speed); //вверх
+                    move();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(speed);
+                    move();
+                }
+                break;
+            case 2:
+                //если нажимаем W - запускается этот код
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(-speed); //вверх
+                    move();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(speed);
+                    move();
+                }
+                break;
+        }
 
     }
+
     public void keyReleased(KeyEvent e) {
-
+        switch (id) {
+            case 1:
+                //0 чтобы не двигалась
+                if (e.getKeyCode() == KeyEvent.VK_W) {
+                    setYDirection(0); //вверх
+                    move();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_S) {
+                    setYDirection(0);
+                    move();
+                }
+                break;
+            case 2:
+                if (e.getKeyCode() == KeyEvent.VK_UP) {
+                    setYDirection(0); //вверх
+                    move();
+                }
+                if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+                    setYDirection(0);
+                    move();
+                }
+                break;
+        }
     }
 
-    //потому что двигаются только вверх и вниз
     public void setYDirection(int yDirection) {
+        yVelocity = yDirection;
 
     }
+
     public void move() {
-
+        y= y + yVelocity;
     }
+
     public void draw(Graphics g) {
-
+        if (id == 1)
+            g.setColor(Color.blue);
+        else
+            g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
     }
-
 }
